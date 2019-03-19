745fc51fbef288aa4e414d8e621c4a3e17f690db
sansona <jiaming.justin.chen@gmail.com>
2019-03-18 17:44:55 -0700

    Added iterative simulation function

--- algorithms.py
+++ algorithms.py
@@ -1,15 +1,19 @@
 import random
 import string
 import time
+from collections import OrderedDict
 
-# ------------------------------------------------------------------------------
+CHARS = list(string.ascii_uppercase +
+             string.ascii_lowercase +
+             string.digits)
+# -----------------------------------------------------------------------------
 
 
 def format_password(password):
     assert password.isalnum()
     return [x for x in password]
 
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def run_brute_force(fpassword):
@@ -17,19 +21,15 @@ def run_brute_force(fpassword):
     Brute force algorithm to "crack" password- generate random strings until
     password is generated
     '''
-    chars = list(string.ascii_uppercase +
-                 string.ascii_lowercase +
-                 string.digits)
-
     start_time = time.time()
 
-    starting_guess = ''.join(random.choice(chars)
+    starting_guess = ''.join(random.choice(CHARS)
                              for x in range(len(fpassword)))
     guess = [x for x in starting_guess]
 
     num_guesses = 0
     while guess != fpassword:
-        guess = list(''.join(random.choice(chars)
+        guess = list(''.join(random.choice(CHARS)
                              for x in range(len(fpassword))))
         num_guesses += 1
 
@@ -37,9 +37,11 @@ def run_brute_force(fpassword):
 
     print('Brute force algorithm\n')
     print('Number attempts: %s' % num_guesses)
-    print('Time: %.3fs\n\n' % (end_time - start_time))
+    print('Time: %.3fs\n' % (end_time - start_time))
 
-# ------------------------------------------------------------------------------
+    return num_guesses, end_time - start_time
+
+# -----------------------------------------------------------------------------
 
 
 def measure_fitness(attempt, original):
@@ -49,7 +51,7 @@ def measure_fitness(attempt, original):
     assert type(attempt) == list and type(original) == list
     return len([i for i, j in zip(attempt, original) if i == j])
 
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def run_genetic_algorithm(fpassword):
@@ -57,15 +59,10 @@ def run_genetic_algorithm(fpassword):
     Utilize hill-climbing algorithm to "crack" password given fitness metric
     of string similarity
     '''
-
-    chars = list(string.ascii_uppercase +
-                 string.ascii_lowercase +
-                 string.digits)
-
     start_time = time.time()
 
-    # start off with random permutation of chars
-    starting_guess = ''.join(random.choice(chars)
+    # start off with random permutation of CHARS
+    starting_guess = ''.join(random.choice(CHARS)
                              for x in range(len(fpassword)))
     guess = [x for x in starting_guess]
     fitness = measure_fitness(guess, fpassword)
@@ -76,7 +73,7 @@ def run_genetic_algorithm(fpassword):
     while guess != fpassword:
         new_guess = guess[:]  # use slice since python shallow copies lists
         mutation_location = random.randint(0, len(guess) - 1)
-        new_guess[mutation_location] = random.choice(chars)
+        new_guess[mutation_location] = random.choice(CHARS)
         new_fitness = measure_fitness(new_guess, fpassword)
         num_guesses += 1
 
@@ -88,21 +85,38 @@ def run_genetic_algorithm(fpassword):
 
     print('Genetic algorithm\n')
     print('Number attempts: %s' % num_guesses)
-    print('Time: %.3fs' % (end_time - start_time))
+    print('Time: %.3fs\n\n' % (end_time - start_time))
 
-# ------------------------------------------------------------------------------
+    return num_guesses, end_time - start_time
+
+# -----------------------------------------------------------------------------
 
 
-def main():
-    pw = 'sr2'
-    password = format_password(pw)
-    run_brute_force(password)
-    run_genetic_algorithm(password)
+def run_simulations(n_iter=5):
+    '''
+    simulates cracking passwords for passwords up to n_iter chars using
+    brute force & hill climbing algorithms. Returns the number of attempts and time for each cracking for both algorithms in OrderedDicts
+    '''
 
-# ------------------------------------------------------------------------------
+    brute_data = OrderedDict()
+    genetic_data = OrderedDict()
 
+    for len_pw in range(1, n_iter):
+        pw = ''.join(random.choice(CHARS) for x in range(len_pw))
+        password = format_password(pw)
+        b_n, btime = run_brute_force(password)
+        gen_n, gtime = run_genetic_algorithm(password)
+
+        brute_data[b_n] = btime
+        genetic_data[gen_n] = gtime
+
+    return brute_data, genetic_data
+
+
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    main()
+    # (TODO): add in Monte Carlo? Plotting?
+    brute, genetic = run_simulations()
 
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
